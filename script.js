34eb11402f4ec4007051f2febf0f838fcebb985e
Zena-art <zena.nazim@gmail.com>
2024-10-03 17:00:28 -0500

    cached elements

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // Variables
+//Cached at least one element using getElementById
 let openBtn = document.getElementById('open-btn');
 let modalContainer = document.getElementById('modal-container');
+let closeBtn = document.getElementById("close-btn");
+// Query Selector for the form and input
+let nameInput = document.querySelector('#name-input'); 
+let nameForm = document.querySelector('#name-form');   
+let guessesList = document.querySelector('#guesses-list'); 
+
+// Event Listeners
+
+openBtn.addEventListener('click', function() {
+  modalContainer.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function() {
+  modalContainer.style.display = 'none';
+});
